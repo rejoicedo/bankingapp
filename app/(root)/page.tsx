ba6a2d709a40f993c68019f4ecb8017d3d3e5ae8
unknown <rejoiceoedokpayi@gmail.com>
2024-10-14 21:54:29 +0100

    Implemented the Sidebar UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = { firstName: 'Rejoice' };
+    const loggedIn = { firstName: 'Rejoice', lastName: 'Edokpayi', email: 'rejoiceoedokpayi@gmail.com' };
 
   return (
     <section className="home">
@@ -22,7 +23,15 @@ const Home = () => {
                     totalCurrentBalance={1250.35}
                 />
             </header>
+
+            RECENT TRANSACTIONS
         </div>
+
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+        />
     </section>
   )
 }
